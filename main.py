3687620b80178a1e4b21fd99c0730f5ae8e8c1cf
Andrew <frintu.andrei07@gmail.com>
2024-01-11 14:07:07 +0000

    functional number limit downloader

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import os
 import time
+import shutil
 import requests
 from art import *
 from bs4 import BeautifulSoup
@@ -14,7 +15,7 @@ _headers_ = {
 }
 
 if __name__ == "__main__":
-    
+
     try:
 
         # print init information
@@ -33,12 +34,48 @@ if __name__ == "__main__":
             os.mkdir("pbinfo")
             print("Directory for solutions was now created!")
         else:
+            shutil.rmtree("pbinfo") # dev only
+            os.mkdir("pbinfo")
             print("Directory for solutions already exists!")
 
         def dl_until(number):
             if 1 <= number <= 5000:
+                _counter_ = 0
                 # will download from 1 to 5000
                 print(f"Downloading {number} sources that are available!")
+                # go from source 0 to source 5000 and check availability
+                for i in range(5000):
+                    # download the source code from pbinfo website
+                    _ev_addr_ = f"https://www.pbinfo.ro/?pagina=solutie-oficiala&id={i}"
+                    response = requests.get(_ev_addr_, headers=_headers_, cookies=_cookies_)
+
+                    if response.status_code == 200:
+
+                        # if solution can be found download it
+                        soup = BeautifulSoup(response.text, 'html.parser') # extract all html code from the page
+                        # select the target element from the page content
+                        target_element = soup.select_one("#zona-mijloc > div > div:nth-child(12) > div.col-lg-9.col-md-9 > pre")
+
+                        # if the target element is available download source code
+                        if target_element:
+                            # save the source code inside variable
+                            extracted_text = target_element.get_text()
+                            # create directory for the new solution
+                            if not os.path.isdir(f"pbinfo/pbinfo-{i}"):
+                                os.mkdir(f"pbinfo/pbinfo-{i}")
+                                # create and write code to file
+                                _file_ = f"pbinfo/pbinfo-{i}/main.cpp"
+                                with open(_file_, "w", encoding="utf-8") as file:
+                                    file.write(extracted_text)
+                                print(f'Downloading source code of the solution for problem with id #{i}')
+                                _counter_ = _counter_ + 1
+                        
+                    if _counter_ == number:
+                        print("The number of solutions you specified was downloaded! Exiting the program.")
+                        exit()
+                    else:
+                        print(f"Number was not hit! {_counter_}")
+                    
             else:
                 raise ValueError("Input must be between 1 and 5000")
 
@@ -76,34 +113,6 @@ if __name__ == "__main__":
             else:
                 print("ERROR: Invalid input. Please enter either one or two numbers.")
 
-        _counter_ = 0
-        if _ssid_ != '':
-            # for loop through all the problems
-            for i in range(_problems_):
-
-                # download the source code from pbinfo website
-                _ev_addr_ = f"https://www.pbinfo.ro/?pagina=solutie-oficiala&id={i}"
-                response = requests.get(_ev_addr_, headers=_headers_, cookies=_cookies_)
-
-                if response.status_code == 200:
-
-                    # if solution can be found download it
-                    soup = BeautifulSoup(response.text, 'html.parser') # extract all html code from the page
-                    # select the target element from the page content
-                    target_element = soup.select_one("#zona-mijloc > div > div:nth-child(12) > div.col-lg-9.col-md-9 > pre")
-
-                    # if the target element is available download source code
-                    if target_element:
-                        # save the source code inside variable
-                        extracted_text = target_element.get_text()
-                        # create directory for the new solution
-                        if not os.path.isdir(f"pbinfo/pbinfo-{i}"):
-                            os.mkdir(f"pbinfo/pbinfo-{i}")
-                            # create and write code to file
-                            _file_ = f"pbinfo/pbinfo-{i}/main.cpp"
-                            with open(_file_, "w", encoding="utf-8") as file:
-                                file.write(extracted_text)
-                            print(f'Downloading source code of the solution for problem with id #{i}')
     except KeyboardInterrupt:
         print("\nCTRL + C pressed! Exiting the program.")
 
